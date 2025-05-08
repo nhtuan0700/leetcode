4ae47c73c8ccfd37ac7e6bef585bfd5b3c7cbaee
TuanNH <nhtuan0700@gmail.com>
2025-05-08 21:43:40 +0700

    init commit

--- 17.monotonic-stack/907.SumOfSubarrayMinimums/main.py
+++ 17.monotonic-stack/907.SumOfSubarrayMinimums/main.py
@@ -15,5 +15,28 @@ class Solution:
                 res = (res + minNum) % (1e9+7)
         return int(res)
 
-# 3 1 2 4
-# 
+# the number of arr[i] in result:
+# = arr[i] * number of subarrays where arr[i] is the minimum
+# = arr[i] * (left+1) * (right+1)
+# left: arr[left_j] > arr[i]
+# right: arr[right_j] >= arr[i]
+
+# _ _ i _  => (2+1)*(1+1) = 6
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        stack = []  # (idx, count)
+        res = 0
+        mod = 1e9 + 7
+        for i, num in enumerate(arr):
+            count = 1
+            while stack and arr[stack[-1][0]] > num:
+                v = stack.pop()
+                count += v[1]
+                res = (res + v[1] * (i - v[0]) * arr[v[0]]) % mod
+            stack.append((i, count))
+
+        while stack:
+            v = stack.pop()
+            res = (res + v[1] * (len(arr) - v[0]) * arr[v[0]]) % mod
+
+        return int(res)
